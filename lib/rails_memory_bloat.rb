965d2a1c7acea83189ce850ea162f0d4c3025cf8
Ernesto Cojulun <erniecojulun@gmail.com>
2016-03-11 12:51:24 -0800

    need reuire

--- lib/rails_memory_bloat.rb
+++ lib/rails_memory_bloat.rb
@@ -1,5 +1,6 @@
 require "rails_memory_bloat/version"
 require "active-record-instance-count"
+require "action_controller"
 #Rails.application.middleware.use(ActiveRecordInstanceCount::Middleware,:logger => Logger.new('/dev/null'))
 
 module ActionController
